2b896f5a98ffc255a6e3d4eadb5d346aa352944f
Ryan Sharkey <rsharkey@kloveair1.com>
2021-12-14 17:58:47 -0800

    Dockerize script

--- list_dir.py
+++ list_dir.py
@@ -2,8 +2,8 @@ import glob
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
- 
-directory_path = '_results_files'
+
+directory_path = 'results_files'
 files = glob.glob(directory_path + '/**/*', recursive=True)
 save_to = 'shortcodes.csv'
 
@@ -12,13 +12,13 @@ data = DataFrame()
 for curfile in files:
     temp = pd.read_csv(curfile)
     try:
-        tmp = temp[['shortcode','edge_media_preview_comment.count','edge_media_preview_like.count', 'edge_media_to_caption.edges']]
+        tmp = temp[['shortcode', 'edge_media_preview_comment.count',
+                    'edge_media_preview_like.count', 'edge_media_to_caption.edges']]
         data = pd.concat([data, tmp])
     except:
         print('empty')
     print(curfile)
 
-data.drop_duplicates(subset=['shortcode'], inplace = True)
- 
+data.drop_duplicates(subset=['shortcode'], inplace=True)
+
 data.to_csv(save_to)
-    
